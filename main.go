9ebbc553a65ca2ea42901e007e7c122f2ae92618
lonegunman <lonegunmanb@hotmail.com>
2019-02-26 17:39:25 +0800

    add build script

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+//go:generate go get github.com/golang/mock/gomock
+//go:generate go install github.com/golang/mock/gomock
+//go:generate go generate ./...
+//go:generate go get -t -v ./...
+
 func main() {
 	clean := flag.Bool("c", false, "clean generated code")
 	containerIdentName := flag.String("cident", "container", "generated container identifier")
